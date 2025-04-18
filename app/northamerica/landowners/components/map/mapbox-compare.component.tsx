10ae8f848e500a4e986291fe1ac203f8d5550e4a
Aren Ashlock <ashlock.aren@gmail.com>
2025-04-18 10:20:53 -0500

    Got my personal MapBox data to appear on NAL

--- app/northamerica/landowners/components/map/mapbox-compare.component.tsx
+++ app/northamerica/landowners/components/map/mapbox-compare.component.tsx
@@ -3,7 +3,7 @@ import mapboxgl, { Map } from 'mapbox-gl';
 import { RefObject, useEffect, useRef, useState } from "react";
 import '@/app/compare.css';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwbnkiLCJhIjoiY20zczc4eXl4MGUybzJpb28xYmNhc3U4ZiJ9.J4GjmS-gRpWINQLTsXCCaw';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJlbi1hc2hsb2NrIiwiYSI6ImNtOHJ5ZW1xeTBuYTMybm9tMDBsNnhqOHAifQ.c7N8HeuFC2GbBwKyaIMCLA';
 
 interface MapboxCompareWrapperProps {
   comparisonContainerRef: RefObject<HTMLDivElement>;
